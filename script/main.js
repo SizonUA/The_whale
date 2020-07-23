d2c15f06a60532a96878b2b4c801e6279858f6f7
Sizon <sizon@vidrada.org>
2020-07-24 00:18:44 +0300

    finish desctop version

--- script/main.js
+++ script/main.js
@@ -1,2 +1,14 @@
-const body = document.querySelector(".body");
-console.log('body: ', body);
+$(document).ready(function () {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  $("a.js-header-button[href='#footer-panel']").click(function () {
+    $("html, body").animate({
+      scrollTop: $(document).height()
+    }, 2000);
+    return false;
+  });
+});
